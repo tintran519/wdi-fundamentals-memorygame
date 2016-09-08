e689c85c92a50bcac8c479c2c29b10a00b2f3bd3
Tin Tran <tin.tran0519@gmail.com>
2016-09-08 13:11:58 -0700

    added if else conditions

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,39 @@ var cardOne = "Queen";
 var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
+
+if(cardTwo===cardFour) {
+  alert("You found a match!"); 
+} else{
+  alert("Sorry,try again.");
+}
+
+if(cardOne===cardThree) {
+  alert("You found a match!"); 
+} else{
+  alert("Sorry,try again.");
+}
+
+if(cardTwo===cardOne) {
+  alert("You found a match!"); 
+} else{
+  alert("Sorry,try again.");
+}
+
+if(cardThree===cardFour) {
+  alert("You found a match!"); 
+} else{
+  alert("Sorry,try again.");
+}
+
+if(cardOne===cardFour) {
+  alert("You found a match!"); 
+} else{
+  alert("Sorry,try again.");
+}
+
+if(cardTwo===cardThree) {
+  alert("You found a match!"); 
+} else{
+  alert("Sorry,try again.");
+}
