47ecaf84f2263cb068f7d2cb7c9b2d37cbe74062
Tin Tran <tin.tran0519@gmail.com>
2016-09-22 18:40:09 -0700

    added ch.11 functions to complete assignment. Added shuffle function.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,40 +1,70 @@
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ['Queen','Queen','King','King'];
+var cardsInPlay = [];
 
-if(cardTwo===cardFour) {
+var gboard = document.getElementById('game-board');
+
+var createBoard = function() {
+  for(var i = 0; i<cards.length; i++) {
+  var cardElement = document.createElement('div');
+  cardElement.className='card';
+  cardElement.setAttribute ('data-card',cards[i]);
+  cardElement.addEventListener('click',isTwoCards);
+  gboard.appendChild(cardElement);
+}
+}
+
+var deleteBoard = function() {
+  for(var i = cards.length; i>0; i-=1) {
+    var playedCards = document.querySelector('.card');
+    gboard.removeChild(playedCards);
+    playedCards.removeEventListener('click',isTwoCards);
+    playedCards.removeAttribute ('data-card',cards[i]);
+  }
+}
+
+var shuffle = function(array) {
+  setTimeout(function() {
+  deleteBoard();
+  createBoard();
+},100);
+
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -=1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var isTwoCards = function() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+  console.log(this.getAttribute('data-card'));
+  if(this.getAttribute('data-card') === 'King') {
+    this.innerHTML = "<img src = 'kingOfHearts.png' style ='width:150px;height:200px;'>";
+  } else {
+    this.innerHTML = "<img src = 'queenOfHearts.png' style ='width:150px;height:200px;'>";
+  }
+  if(cardsInPlay.length ===2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+var isMatch = function(activecards) {
+  if(activecards[0]=== activecards[1]) {
   alert("You found a match!"); 
 } else{
   alert("Sorry,try again.");
 }
-
-if(cardOne===cardThree) {
-  alert("You found a match!"); 
-} else{
-  alert("Sorry,try again.");
+  setTimeout(function() {
+  deleteBoard();
+  createBoard();
+}, 1000);
 }
 
-if(cardTwo===cardOne) {
-  alert("You found a match!"); 
-} else{
-  alert("Sorry,try again.");
-}
-
-if(cardThree===cardFour) {
-  alert("You found a match!"); 
-} else{
-  alert("Sorry,try again.");
-}
-
-if(cardOne===cardFour) {
-  alert("You found a match!"); 
-} else{
-  alert("Sorry,try again.");
-}
-
-if(cardTwo===cardThree) {
-  alert("You found a match!"); 
-} else{
-  alert("Sorry,try again.");
-}
+createBoard()
